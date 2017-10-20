e67cb237d4d8dfb83ef988905c1f4cdff73e4d12
Daniel Plewinski <dplewinski@gmail.com>
2017-10-20 16:18:36 +0200

    It is now possible to view users and addresses

--- class/User.php
+++ class/User.php
@@ -2,5 +2,119 @@
 
 class User implements Action
 {
+    /**
+     * @var DBmysql $db
+     */
+
+    public static $db;
+
+    private $id = -1;
+    private $name = '';
+    private $surname = '';
+    private $credits = 0;
+    private $address = '';
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getCredits(): int
+    {
+        return $this->credits;
+    }
+
+    public function setCredits(int $credits)
+    {
+        $this->credits = $credits;
+
+        return $this;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public static function loadAll()
+    {
+        $sql = "SELECT * FROM User";
+        self::$db->query($sql);
+        $allFromDb = self::$db->resultSet();
+
+        foreach ($allFromDb as $user) {
+            // wypelniamy tablicę elementami z bazy
+            $userList[] = [
+                'id' => $user['id'],
+                'name' => $user['name'],
+                'surname' => $user['surname'],
+                'credits' => $user['credits'],
+                'address' => $user['address']
+            ];
+        }
+        return $userList;
+    }
+
+    // te metody trzeba uzupełnić
+
+    public function save()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+
+    public static function load($id = null)
+    {
+
+
+    }
+
+
+    public static function setDb(Database $db)
+    {
+        self::$db = $db;
+    }
+
 
 }
--- restEndPoints/User.php
+++ restEndPoints/User.php
@@ -1,7 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mbp12
- * Date: 20.10.2017
- * Time: 13:31
- */
+
+User::setDb($db);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') { // Pobieramy (REST)
+
+    $users = User::loadAll();
+
+    echo json_encode($users); // zwracamy json do frontendu - echo bo się nie da zwrócić
+
+    exit();
+
+
+    // kod poniżej trzeba uzupełnić bo dodałem tylko kopie z innego pliku
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') { // Dodajemy (REST)
+
+    // tworzymy objekt
+    $size = new Size();
+
+    // ustawiamy własności
+    $size->setPrice($_POST['price']);
+    $size->setName($_POST['size']);
+
+    // zapisujemy do bazy
+    $size->save();
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
+    parse_str(file_get_contents("php://input"), $patchVars);
+    $sizeToUpdate = Size::load($patchVars['id']);
+    $sizeToUpdate->setPrice($patchVars['price']);
+    $sizeToUpdate->setName($patchVars['size']);
+    $sizeToUpdate->update();
+
+
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+
+    parse_str(file_get_contents("php://input"), $deleteVars);
+    $sizeToDelete = Size::load($deleteVars['id']); // zwraca pojedynczy objekt Size
+    $sizeToDelete->delete();
+
+}
